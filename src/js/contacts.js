82b666dfc20f5f07a57cafc648051e8aa9c86aa3
Yuliya Chukhil <yuliya.chukhil@gmail.com>
2020-11-29 16:08:45 +0200

    rock-paper-scissors js code added

--- src/js/contacts.js
+++ src/js/contacts.js
@@ -1,1 +1,39 @@
 import css from "../css/contacts.css";
+import contactsRef from "./refs.js";
+const { contactsSection } = contactsRef;
+
+const game = ["rock", "paper", "scissors"];
+
+function createButtons(arr) {
+  return arr.map((el) => {
+    const button = document.createElement("button");
+    button.textContent = el;
+    button.dataset.key = el;
+    return button;
+  });
+}
+
+const buttons = createButtons(game);
+contactsSection.append(...buttons);
+
+let index = Math.round(Math.random() * (game.length - 1));
+const result = document.createElement("p");
+contactsSection.append(result);
+
+contactsSection.addEventListener("click", (evt) => {
+  let user = evt.target.dataset.key;
+  let computer = game[index];
+  console.log(`User: ${user} - Computer: ${computer}`);
+
+  let msg;
+  if (user === computer) {
+    msg = "Tied score!";
+  } else {
+    (user === "scissors" && computer === "paper") ||
+    (user === "paper" && computer === "rock") ||
+    (user === "rock" && computer === "scissors")
+      ? (msg = "User wins =)")
+      : (msg = "Computer wins =(");
+  }
+  result.textContent = msg;
+});
