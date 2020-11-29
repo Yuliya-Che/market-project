f92e3b2ee36e3dc3f59ead320b0cd9ee994b21da
Yuliya Chukhil <yuliya.chukhil@gmail.com>
2020-11-29 12:06:01 +0200

    base structure added

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import css from "./css/styles.css";
+
+import "./js/home.js";
+import "./js/about.js";
+import "./js/contacts.js";
---
+++ src/js/refs.js
@@ -0,0 +1,9 @@
+export default {
+  header: document.querySelector(".header"),
+  nav: document.querySelector(".nav"),
+  ul: document.querySelector(".navList"),
+
+  homeSection: document.querySelector(".home"),
+  aboutSection: document.querySelector(".about"),
+  contactsSection: document.querySelector(".contacts"),
+};
